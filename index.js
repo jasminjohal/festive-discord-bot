ed63df9d35951187c9420c62b115c86d45683380
jjrx <jjohal52@gmail.com>
2022-04-20 01:27:27 -0700

    removed command interaction

--- index.js
+++ index.js
@@ -9,16 +9,6 @@ const TOKEN = process.env.DISCORD_TOKEN;
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
   const guild = client.guilds.cache.get(GUILD_ID);
-  if (guild) {
-    commands = guild.commands;
-  } else {
-    commands = client.application?.commands;
-  }
-
-  commands?.create({
-    name: "ping",
-    description: "Replies with pong.",
-  });
 });
 
 client.on("message", async (message) => {
@@ -32,14 +22,6 @@ client.on("message", async (message) => {
         "There was an error setting the guild name and icon!"
       );
     }
-  }
-});
-
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) return;
-
-  if (interaction.commandName === "ping") {
-    await interaction.reply("Pong!");
   }
 });
 
